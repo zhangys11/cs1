cc21bc9fbd760df313b1cce98ffa77bb42799d49
zhangys11 <oo@zju.edu.cn>
2022-05-06 12:01:40 +0800

    Update spcs.py

--- src/spcs.py
+++ src/spcs.py
@@ -28,6 +28,8 @@ def dctmtx(m, n, display = False):
         plt.figure()
         plt.imshow(mtx, interpolation='nearest', cmap=cm.Greys_r)
         plt.axis('off')
+        plt.title("DCT (" + str(m) + " , " + str(n) + ")")
+        plt.show()
     
     return mtx
 
@@ -42,7 +44,7 @@ def img_dct(path):
     w,h = img.shape
     imgdata = img.reshape(1, w*h) # expand to 1D array
 
-    mtx = dctmtx(w,h, True)
+    mtx = dctmtx(w, h, True)
 
     # dctmtx * dctmtx' = E
 
@@ -102,4 +104,115 @@ def img_dct(path):
     plt.show()
 
     v = cv2.Laplacian(np.log(abs(dst)), cv2.CV_64F).var() # Blur Detection using the variance of the Laplacian method
-    print('Laplacian var (Blur Detection) of DCT = ', v)
+    print('Laplacian var (Blur Detection) of DCT = ', round(v,3) )
+    v2 = cv2.Laplacian(np.log(abs(dst2)), cv2.CV_64F).var() # Blur Detection using the variance of the Laplacian method
+    print('Laplacian var (Blur Detection) of DCT (cv2) = ', round(v2,3) )
+
+def dftmtx(N, flavor = 1, display = True):    
+
+    if flavor == 2:
+        i, j = np.meshgrid(np.arange(N), np.arange(N))
+        w = np.exp( - 2 * np.pi * 1j / N )
+        mtx = np.power( w, i * j ) / np.sqrt(N)
+
+    else:
+        mtx = np.zeros((N,N), dtype=np.complex)
+        w = np.exp(-2 * np.pi * 1j / N) # python uses j as imaginary unit
+
+        for j in range(N):
+            for k in range(N):
+                mtx[j, k] = np.power(w, k*j) / np.sqrt(N)
+
+    if display:
+        f,ax = plt.subplots(1,3,figsize=(4*3,4))
+
+        f.suptitle("DFT Sensing Matrix using Flavor " + str(flavor))
+
+        plt.subplot(1,3,1)
+        plt.imshow(abs(mtx), interpolation='nearest', cmap=cm.Greys_r)  
+        plt.axis('off')
+        ax[0].set_title('abs')
+        
+        plt.subplot(1,3,2)
+        plt.imshow(np.angle(mtx), interpolation='nearest', cmap=cm.Greys_r)
+        plt.axis('off')   
+        ax[1].set_title('phase')
+
+        plt.subplot(1,3,3)
+        E = np.dot(mtx, mtx)
+        plt.imshow(abs(E), cmap=cm.Greys_r)
+        plt.axis('off')
+        ax[2].set_title('DFT @ DFT.T = I')
+    
+    return mtx
+
+def img_dft(path):
+
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE).astype('float') # input a square image
+    w,h = img.shape
+    s = min(w,h)
+    img = img[:s,:s]
+    print('image cropped to ', str(s), " x ", str(s) )
+    imgdata = img.reshape(1, s*s) # expand to 1D array
+
+    mtx = dftmtx(s, True)
+
+    E = np.dot(mtx, np.transpose(mtx))
+    plt.imshow(np.abs(E), cmap=cm.Greys_r)
+    plt.title('DFT @ DFT.T = I')
+    plt.axis('off')
+    plt.show()
+
+    dst = np.dot(mtx , img)
+    dst = np.dot(dst, np.transpose(mtx))
+    dst2 = cv2.dft(img) # Method 2 - using opencv 
+
+    # assert np.allclose(dst, dst2)
+
+    # IDCT to reconstruct image
+
+    # Method 1
+    img_r = np.dot(np.transpose(mtx) , dst)
+    img_r = np.abs( np.dot(img_r, mtx) )
+
+    # Method 2 - opencv
+    img_r2 = cv2.idft(dst2)
+
+    plt.figure(figsize=(12,8))
+
+    plt.subplot(231)
+    plt.imshow(img, 'gray')
+    plt.title('original image')
+    plt.xticks([]), plt.yticks([])
+    
+    plt.subplot(232)
+    plt.imshow(np.log(abs(dst)),'gray')
+    plt.title('DFT')
+    plt.xticks([]), plt.yticks([])
+    
+    plt.subplot(233)
+    plt.imshow(img_r, 'gray')
+    plt.title('IDFT')
+    plt.xticks([]), plt.yticks([])
+    
+    plt.subplot(234)
+    plt.imshow(img, 'gray')
+    plt.title('original image')
+    plt.axis('off')
+    
+    plt.subplot(235)
+    plt.imshow(np.log(abs(dst2)),'gray')
+    plt.title('DFT(cv2)')
+    plt.axis('off')
+
+    plt.subplot(236)
+    plt.imshow(img_r2,'gray')
+    plt.title('IFCT(cv2)')
+    plt.axis('off')
+
+    plt.show()
+
+    v = cv2.Laplacian(np.log(abs(dst)), cv2.CV_64F).var() # Blur Detection using the variance of the Laplacian method
+    print('Laplacian var (Blur Detection) of DFT = ', round(v,3) )
+    v2 = cv2.Laplacian(np.log(abs(dst2)), cv2.CV_64F).var() # Blur Detection using the variance of the Laplacian method
+    print('Laplacian var (Blur Detection) of DFT (cv2) = ', round(v2,3) )
